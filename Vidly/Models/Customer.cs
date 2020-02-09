81d1ed3973ce67d8154512691eac3d8d3f34585b
rodrigofontes15 <rodrigofontes15@users.noreply.github.com>
2020-02-09 19:27:30 +0100

    fixing magic numbers

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -22,6 +22,8 @@ namespace Vidly.Models
         public byte MembershipTypeId { get; set; }
 
         [Display(Name="Date of Birth")]
+
+        [Min18YearsIfAMember]
         public DateTime? Birthdate { get; set; }
     }
 }
